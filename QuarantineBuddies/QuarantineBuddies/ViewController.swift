b0750a43efc213a34fc5ba6499add0b59f282953
Jamie Huang <jamie.huang@gmail.com>
2020-09-11 18:36:35 -0700

    Add files via upload

---
+++ QuarantineBuddies/QuarantineBuddies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuarantineBuddies
+//
+//  Created by Jamie Huang on 9/11/20.
+//  Copyright © 2020 Jamie Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
