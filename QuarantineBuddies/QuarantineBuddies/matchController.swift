8735c20f1f54f9d44324d09266cfc2cf8c10a297
Jamie Huang <jamie.huang@gmail.com>
2020-09-12 00:18:23 -0700

    long/lat updated

--- QuarantineBuddies/QuarantineBuddies/matchController.swift
+++ QuarantineBuddies/QuarantineBuddies/matchController.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import CoreLocation
+import UIKit
+
+class matchController: UIViewController, CLLocationManagerDelegate {
+    
+    private var locationManager:CLLocationManager?
+    
+     private let latLngLabel: UILabel = {
+           let label = UILabel()
+           label.backgroundColor = .systemFill
+           label.numberOfLines = 0
+           label.textAlignment = .center
+           label.font = .systemFont(ofSize: 26)
+           return label
+       }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        latLngLabel.frame = CGRect(x: 20, y: view.bounds.height / 2 - 380, width: view.bounds.width - 40, height: 60)
+        view.addSubview(latLngLabel)
+        
+        locationManager = CLLocationManager()
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.startUpdatingLocation()
+        locationManager?.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            latLngLabel.text = "Lat: \(location.coordinate.latitude) \nLng: \(location.coordinate.longitude)"
+            latLngLabel.font = UIFont(name: "Avenir", size: 15.0)
+        }
+    }
+}
+
+
